5aef89aa649e2c26cc42225615a590dff184656d
Ferran Illa Capellas <ferran.illa26@gmail.com>
2017-11-04 11:32:23 +0100

    Headers de Dijkstra i BFS agregats

---
+++ src/BFS.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "Vector2D.h"
+
+namespace BFS {
+	//no son les funcions de veritat eh, es un exemple
+	void expandFrontier() {}
+	void addToVisited() {}
+	bool isTarget() {}
+}
--- src/Dijkstra.h
+++ src/Dijkstra.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <vector>
 #include "Vector2D.h"
 
 namespace dijkstra {
