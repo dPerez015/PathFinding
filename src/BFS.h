8cc727bd85e51198fdcc5b3c440f7cd21bbd87d4
Ferran Illa Capellas <ferran.illa26@gmail.com>
2017-11-08 00:14:11 +0100

    clase de BFS preparada per implementar (i corretgir :P)

--- src/BFS.h
+++ src/BFS.h
@@ -23,9 +23,21 @@ struct graph {
 
 class BFS {
 private:
+	graph worldGraph;
+	queue<Node> frontier;
+	vector<Node> visited;
+public:
+	BFS(); //inicialitza la frontera amb el node de la posicio inicial (i el graph)
+	~BFS();
 
-	//no son les funcions de veritat eh, es un exemple
-	void expandFrontier() {}
-	void addToVisited() {}
-	bool isTarget() {}
+	void search(); //funcio principal, si frontier!=0;
+	bool isTarget();//si es target->break
+	void expandFrontier(Node n); //afageix a la frontera els veins de n
+	void addToVisited(Node n); //afageix n a visited
+	
+
+	graph getGraph();
+	queue<Node> getFrontier();
+	vector<Node> getVisited();
+
 };
