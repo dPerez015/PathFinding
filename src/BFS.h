57ea3891cf1345ceaaacaa7517e494030f7a1738
Ferran Illa Capellas <ferran.illa26@gmail.com>
2017-11-08 01:04:21 +0100

    commit anterior 2.0 BFS

--- src/BFS.h
+++ src/BFS.h
@@ -3,41 +3,42 @@
 #include <vector>
 #include "Vector2D.h"
 #include <queue>
+#include "Node.h"
 
 using namespace std;
 
-struct Node {
+struct NodeF {
 	Vector2D position;
 	float cost;
 };
 
-struct Connection {
-	Node fromNode, toNode;
+struct ConnectionF {
+	NodeF fromNode, toNode;
 	float cost;
 };
 
-struct graph {
-	vector<Node> n;
-	vector<Connection> c;
+struct graphF {
+	vector<NodeF> n;
+	vector<ConnectionF> c;
 };
 
 class BFS {
 private:
-	graph worldGraph;
-	queue<Node> frontier;
-	vector<Node> visited;
+	graphF worldGraph;
+	queue<NodeF> frontier;
+	vector<NodeF> visited; //amb struct Node del david si que es pot fer, sino shan de guardar les connexions (el node anterior)
 public:
 	BFS(); //inicialitza la frontera amb el node de la posicio inicial (i el graph)
 	~BFS();
 
 	void search(); //funcio principal, si frontier!=0;
 	bool isTarget();//si es target->break
-	void expandFrontier(Node n); //afageix a la frontera els veins de n
-	void addToVisited(Node n); //afageix n a visited
+	void expandFrontier(NodeF n); //afageix a la frontera els veins de n
+	void addToVisited(NodeF n); //afageix n a visited
 	
 
-	graph getGraph();
-	queue<Node> getFrontier();
-	vector<Node> getVisited();
+	graphF getGraph();
+	queue<NodeF> getFrontier();
+	vector<NodeF> getVisited();
 
 };
