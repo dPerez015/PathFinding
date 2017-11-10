dfa6577e028b45707a9a40f365ebecee02adafc8
david lopez saludes <davidlopez1972@hotmail.es>
2017-11-10 19:31:46 +0100

    Almost

--- src/Dijkstra.cpp
+++ src/Dijkstra.cpp
@@ -6,6 +6,23 @@ std::vector<Vector2D> dijkstra::path;
 bool dijkstra::notFound;
 bool dijkstra::notInCostSoFar;
 
+void dijkstra::draw() {
+	it = frontier.begin();
+	if (it != frontier.end()) {
+		draw_circle(TheApp::Instance()->getRenderer(), it->second->position.x, it->second->position.y, TheApp::Instance()->getGridCellSize().x / 2, 0, 255, 0, 255);
+		//SDL_RenderDrawLine(TheApp::Instance()->getRenderer(), it->second->position.x, it->second->position.y, it->second->previousNode->position.x, it->second->previousNode->position.y);
+		it++;
+	}
+	while (it != frontier.end()) {
+		draw_circle(TheApp::Instance()->getRenderer(), it->second->position.x, it->second->position.y, TheApp::Instance()->getGridCellSize().x / 2, 255, 255, 0, 255);
+		//SDL_RenderDrawLine(TheApp::Instance()->getRenderer(), it->second->position.x, it->second->position.y, it->second->previousNode->position.x, it->second->previousNode->position.y);
+		it++;
+	}
+	for (int i = 0; i < path.size(); i++) {
+		draw_circle(TheApp::Instance()->getRenderer(), path[i].x, path[i].y, TheApp::Instance()->getGridCellSize().x / 2, 255, 0, 0, 255);
+	}
+}
+
 void dijkstra::fillPath(std::vector<Vector2D> path, Node* end) {
 	while (end != nullptr) {
 		path.push_back(end->position);
@@ -19,11 +36,25 @@ void dijkstra::fillPath(std::vector<Vector2D> path, Node* end) {
 	}
 }
 
+bool dijkstra::checkFrontier(Node* pos, std::multimap<float, Node*> frontier) {
+
+	std::multimap<float, Node*>::iterator checker;
+
+	for (checker = frontier.begin(); checker != frontier.end(); checker++) {
+		if (checker->first == pos->pes) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
 std::vector<Vector2D> dijkstra::applyDijkstra(Node* startNode, Vector2D endPos) {
 	frontier.clear();
 	path.clear();
 	startNode->previousNode = nullptr;
-	frontier.emplace(0.0f, startNode->previousNode);
+	frontier.emplace(0.0f, startNode);
+	float temp;//comprovarà que el valor de la frontera sigui més petit que aquest
 	bool notFound = true;
 	bool notInCostSoFar;
 	std::pair <std::multimap<float, Node*>::iterator, std::multimap<float, Node*>::iterator> range;
@@ -37,17 +68,20 @@ std::vector<Vector2D> dijkstra::applyDijkstra(Node* startNode, Vector2D endPos) 
 			else {
 				//frontier.insert(std::pair<float,Node*>(it->second->conexiones[i]->pes, it->second->conexiones[i]->position));
 				range = frontier.equal_range(it->second->conexiones[i]->pes);
-				notInCostSoFar = true;
+				temp = it->first+it->second->conexiones[i]->pes;
+				notInCostSoFar = checkFrontier(it->second->conexiones[i], frontier);
 
-				for (auto j = range.first; j != range.second; ++j) {
-					if (j->second->pes == j->second->conexiones[i]->pes) {
-						notInCostSoFar = false;
-					}
+				//for (auto j = range.first; j != range.second; ++j) {
+				//	if (j->second->pes != j->second->conexiones[i]->pes) {
+				//		notInCostSoFar = false;
+				//	}
+				//}
+				//Mirar en una funció si la posició està a la frontera i retornar un bool 
+				if (notInCostSoFar || temp < it->second->conexiones[i + 1]->pes) {
+					//it->first(temp);
+					frontier.insert(std::pair<float, Node*>(it->first + it->second->conexiones[i]->pes, it->second->conexiones[i]));
 				}
-				if (notInCostSoFar) {
-					frontier.insert(std::pair<float, Node*>(it->second->conexiones[i]->pes, it->second->conexiones[i]));
-				}
-			}//afegir key utilitzant pesis al multimap
+			}
 		}
 		it = frontier.erase(it);
 	}
--- src/Dijkstra.h
+++ src/Dijkstra.h
@@ -5,6 +5,8 @@
 #include <queue>
 #include "Vector2D.h"
 #include "Node.h"
+#include "utils.h"
+#include "SDL_SimpleApp.h"
 
 struct dijkstra {
 private:
@@ -17,6 +19,7 @@ private:
 public:
 
 	static void fillPath(std::vector<Vector2D>, Node*);
+	static bool checkFrontier(Node*, std::multimap<float, Node*>);
 	static std::vector<Vector2D> applyDijkstra(Node*,Vector2D);
-	
+	static void draw();
 };
