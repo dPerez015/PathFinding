7a1b1ebfa4e0426c3f4cb5f163cb8a7fa481a50e
dPerez015 <davidperezgallego@enti.cat>
2017-11-16 14:25:35 +0100

    heuristics improved

--- src/Heuristics.h
+++ src/Heuristics.h
@@ -12,7 +12,18 @@ struct Heuristics {
 	static int manhatanDistance(Vector2D init, Vector2D fin) {
 		/*fin = pix2cell(fin);
 		init = pix2cell(init);*/
-		//return abs(init.x - fin.x) + abs(init.y - fin.y);
-		return 1;
+		int ret1 = fin.x - init.x;
+		if (abs(ret1>=(SRC_WIDTH / (CELL_SIZE*2)))) {
+			if (ret1 < 0) {
+				return abs((fin.x + (SRC_WIDTH / CELL_SIZE))-init.x) + abs(fin.y - init.y);
+			}
+			else {
+				return abs(fin.x - (init.x+(SRC_WIDTH / CELL_SIZE))) + abs(fin.y - init.y);
+			}
+		}
+		else {
+			return abs(ret1) + abs(init.y - fin.y);
+		}
+		
 	}
 };
