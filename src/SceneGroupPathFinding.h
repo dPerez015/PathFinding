ee710525e049c2b160cb1cec256c924122383c2c
Ferran Illa Capellas <ferran.illa26@gmail.com>
2017-11-20 20:52:37 +0100

    la escena per l'exercic 2 i canvis al main. Ja podeu fer commits

---
+++ src/SceneGroupPathFinding.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <vector>
+#include <time.h>
+#include "Scene.h"
+#include "Agent.h"
+#include "Path.h"
+#include "list"
+#include "Node.h"
+#include "utils.h"
+#include "Aestrella.h"
+
+class SceneGroupPathFinding :
+	public Scene
+{
+public:
+	SceneGroupPathFinding();
+	~SceneGroupPathFinding();
+	void update(float dtime, SDL_Event *event);
+	void draw();
+	const char* getTitle();
+private:
+	//graph creation
+	std::vector<std::vector<Node>> graph;
+	int wallsOnCollumn(int);
+	int wallsonCollumn(int, int);
+	int v, h;
+	int v1, h1, numDelNodes;
+	void createGraphPorPasos();
+	void deleteNodesPorPaso();
+	Node* findInGraph(Vector2D);
+	void createGraph();
+	//debugGraph
+	void drawGraph();
+
+	//debug busqueda
+	void drawGraphConexions();
+
+	std::vector<Agent*> agents;
+	Vector2D coinPosition;
+	Vector2D currentTarget;
+	int currentTargetIndex;
+	Path path;
+	int num_cell_x;
+	int num_cell_y;
+	bool draw_grid;
+	std::vector<SDL_Rect> maze_rects;
+	void drawMaze();
+	void drawCoin();
+	SDL_Texture *background_texture;
+	SDL_Texture *coin_texture;
+	void initMaze();
+	bool loadTextures(char* filename_bg, char* filename_coin);
+	std::vector< std::vector<int> > terrain;
+	Vector2D cell2pix(Vector2D cell);
+	Vector2D pix2cell(Vector2D pix);
+	bool isValidCell(Vector2D cell);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "SDL_SimpleApp.h"
 #include "ScenePathFinding.h"
 #include "SceneDebugPF.h"
+#include "SceneGroupPathFinding.h"
 
 #define FRAMES_PER_SEC 30
 
@@ -52,6 +53,9 @@ int main(int argc, char ** argv)
 			}
 			if (event.key.keysym.scancode == SDL_SCANCODE_3)
 			{
+				delete(curr_scene);
+				curr_scene = new SceneGroupPathFinding;
+				app->setWindowTitle(curr_scene->getTitle());
 			}
 			if ((event.key.keysym.scancode == SDL_SCANCODE_Q) || (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE))
 			{
